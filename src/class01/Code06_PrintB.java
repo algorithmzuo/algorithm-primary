be1fee75aa0eb6f115f307da165332b83f4b41e4
左程云 <algorithmzuo@163.com>
2020-12-09 22:14:18 +0800

    modify code on class

---
+++ src/class01/Code06_PrintB.java
@@ -0,0 +1,65 @@
+package class01;
+
+public class Code06_PrintB {
+
+	public static void print(int num) {
+		for (int i = 31; i >= 0; i--) {
+			System.out.print((num & (1 << i)) == 0 ? "0" : "1");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		// 32位
+//		int num = 4;
+//
+//		print(num);
+//		
+//		
+//		int test = 1123123;
+//		print(test);
+//		print(test<<1);
+//		print(test<<2);
+//		print(test<<8);
+//		
+//		
+//		int a = Integer.MAX_VALUE;
+//		System.out.println(a);
+
+//		print(-1);
+//		int a = Integer.MIN_VALUE;
+//		print(a);
+
+//		int b = 123823138;
+//		int c = ~b;
+//		print(b);
+//		print(c);
+
+//		print(-5);
+
+//		System.out.println(Integer.MIN_VALUE);
+//		System.out.println(Integer.MAX_VALUE);
+
+//		int a = 12319283;
+//		int b = 3819283;
+//		print(a);
+//		print(b);
+//		System.out.println("=============");
+//		print(a | b);
+//		print(a & b);
+//		print(a ^ b);
+
+//		int a = Integer.MIN_VALUE;
+//		print(a);
+//		print(a >> 1);
+//		print(a >>> 1);
+//		
+//		int c = Integer.MIN_VALUE;
+//		int d = -c ;
+//		
+//		print(c);
+//		print(d);
+
+	}
+
+}
---
+++ src/class01/Code07_Sort.java
@@ -0,0 +1,79 @@
+package class01;
+
+public class Code07_Sort {
+
+	public static void swap(int[] arr, int i, int j) {
+		int tmp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = tmp;
+	}
+
+	public static void selectSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		int N = arr.length;
+		for (int i = 0; i < N; i++) {
+			int minValueIndex = i;
+			for (int j = i + 1; j < N; j++) {
+				minValueIndex = arr[j] < arr[minValueIndex] ? j : minValueIndex;
+			}
+			swap(arr, i, minValueIndex);
+		}
+	}
+
+	public static void bubbleSort(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		int N = arr.length;
+		for (int end = N - 1; end >= 0; end--) {
+			for (int second = 1; second <= end; second++) {
+				if (arr[second - 1] > arr[second]) {
+					swap(arr, second - 1, second);
+				}
+			}
+		}
+	}
+
+	public static void insertSort1(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		int N = arr.length;
+		for (int end = 1; end < N; end++) {
+			int newNumIndex = end;
+			while (newNumIndex - 1 >= 0 && arr[newNumIndex - 1] > arr[newNumIndex]) {
+				swap(arr, newNumIndex - 1, newNumIndex);
+				newNumIndex--;
+			}
+		}
+	}
+
+	public static void insertSort2(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		int N = arr.length;
+		for (int end = 1; end < N; end++) {
+			for (int pre = end - 1; pre >= 0 && arr[pre] > arr[pre + 1]; pre--) {
+				swap(arr, pre, pre + 1);
+			}
+		}
+	}
+
+	public static void printArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int[] arr = { 7, 1, 3, 5, 1, 6, 8, 1, 3, 5, 7, 5, 6 };
+		printArray(arr);
+		insertSort2(arr);
+		printArray(arr);
+	}
+
+}
