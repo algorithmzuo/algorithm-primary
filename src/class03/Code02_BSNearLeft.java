894185fe6ce447edd6ea825d461b870f72cc0f14
mutouaimaomi <65887709+mutouaimaomi@users.noreply.github.com>
2021-12-10 15:18:53 +0800

    Update Code02_BSNearLeft.java
     //如果num不存在 array[mid]>=num时ans=mid,
     // 那么ans就不会是-1
    if (arr[mid] >= num) {
                    ans = mid;
                    R = mid - 1;
                } else {
                    L = mid + 1;
                }

--- src/class03/Code02_BSNearLeft.java
+++ src/class03/Code02_BSNearLeft.java
@@ -13,14 +13,15 @@ public class Code02_BSNearLeft {
 		int R = arr.length - 1;
 		int ans = -1;
 		while (L <= R) {
-			int mid = (L + R) / 2;
-			if (arr[mid] >= num) {
-				ans = mid;
-				R = mid - 1;
-			} else {
-				L = mid + 1;
-			}
-		}
+            	    int mid = (L + R) / 2;
+                    if (array[mid] == num) {
+                         ans = mid;
+                         R = mid - 1;
+                     } else if (array[mid] < num) {
+                         L = mid + 1;
+                     } else {
+                         R = mid - 1;
+                }
 		return ans;
 	}
 
